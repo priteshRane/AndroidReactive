4a07a11d15ec94947f5b6f6e84eb722b71d27922
Pritesh_PC <priteshrane01@gmail.com>
2020-06-27 08:38:52 +0530

    Dependancies are added, Project setup

--- app/src/main/java/com/ransoft/androidreactive/MainActivity.kt
+++ app/src/main/java/com/ransoft/androidreactive/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.ransoft.androidreactive
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ransoft.androidreactive.coadingwithmitch.HomeActivity
+import com.ransoft.androidreactive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnCodeWithMitch.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
